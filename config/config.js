82a2d4d0f54be63cc87f7cd8b4df4fbc26c22703
adityaFlixir <94819768+adityaFlixir@users.noreply.github.com>
2022-01-27 14:52:42 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   serverPort: process.env.PORT || 4001,
-  dbUrl: "mongodb://localhost:27017/m2m" ,
+  dbUrl: "mongodb+srv://Aditya:Aditya1420@cluster0.f50qp.mongodb.net/m2m?retryWrites=true&w=majority",
   jwt: {
     TokenLife: 86400,
     secret: "M2M",
